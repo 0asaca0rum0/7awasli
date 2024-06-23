606d5543369d8605eb2d8c2ccdf9023b961a0f5d
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2024-06-23 23:47:09 +0100

    trying dynamic routing with profile route

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,55 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Button } from "react-native-paper";
 import { Tabs } from "expo-router";
 import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
-import { FontAwesome } from "@expo/vector-icons";
-import { act } from "react-test-renderer";
+
 import MyTabBar from "@/components/support/tabbar";
 const Tab = createMaterialBottomTabNavigator();
 
 const _layout = () => {
 	return (
-		<Tabs
-			tabBar={props => <MyTabBar {...props}/> }
-			screenOptions={{
-					
-				tabBarLabelStyle: {
-					fontSize: 10,
-					fontWeight: "bold",
-					
-				},
-				tabBarShowLabel: false,
-				tabBarActiveTintColor: "white",
-				tabBarInactiveTintColor: "grey",
-				tabBarActiveBackgroundColor: "#17B139",
-				
-				tabBarItemStyle: {
-					
-					borderTopLeftRadius: 26,
-					borderTopRightRadius: 26,
-					borderBottomLeftRadius: 26,
-					borderBottomRightRadius: 26,
-					width: "10%",
-					
-				},
-				tabBarStyle: {
-					
-					left: "2.5%",
-					right: "2.5%",
-
-					width: "95%",
-					backgroundColor: "#ffffff",
-					borderTopWidth: 0,
-					marginBottom: 8,
-					position: "absolute",
-					borderTopLeftRadius: 26,
-					borderTopRightRadius: 26,
-					borderBottomLeftRadius: 26,
-					borderBottomRightRadius: 26,
-				},
-			}}
-		>
+		<Tabs tabBar={(props) => <MyTabBar {...props} />}>
 			<Tabs.Screen
 				name="index"
 				options={{
@@ -63,8 +21,8 @@ const _layout = () => {
 			<Tabs.Screen
 				name="map"
 				options={{
-					
 					title: "Map",
+					headerShown: false,
 
 					headerTitleAlign: "center",
 				}}
